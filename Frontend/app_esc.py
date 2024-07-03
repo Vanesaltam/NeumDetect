3eb2f5a587bc4a1ccd146cf4bb20aff6fabb2f50
Pablo Zuval <pablo.zuniga.vale@gmail.com>
2024-07-03 00:25:09 -0400

    💄 Update frontend strucure

--- Frontend/app_esc.py
+++ Frontend/app_esc.py
@@ -39,9 +39,9 @@ MODELS = {
 
 # Función para mostrar la imagen centrada
 def display_centered_image(image_path, width=300):
-    col1, col2, col3 = st.columns([1,2,1])
+    col1, col2, col3 = st.columns([1,3,1])
     with col2:
-        st.image(image_path, width=width)
+        st.image(image_path, width=width,use_column_width=False)
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 logo_path = os.path.join(current_dir, '..', 'img', 'LOGO2.png')
@@ -53,28 +53,46 @@ display_centered_image(logo_path, width=300)  # Ajusta la ruta y el ancho según
 st.title("NeumDetect: Detección de Neumonía")
 st.write("Plataforma de detección de neumonía basada en imágenes de rayos X de tórax")
 
-# Carga de imagen
-uploaded_file = st.file_uploader("Cargar imagen de rayos X", type=["jpg", "jpeg", "png"])
+col1, col2, col3 = st.columns([1, 1, 1])
+with col1:
+    st.subheader("Acerca de NeumDetect")
+    st.write("NeumDetect es una aplicación web que utiliza modelos de clasificación de redes neuronales para detectar la presencia de neumonía en imágenes de rayos X de tórax.")
+    st.write("Los modelos disponibles son VGG16, ResNet50, (...).")
+    st.write("La aplicación fue desarrollada como parte de un proyecto de aprendizaje automático.")
+    # st.write("Para obtener más información, visita el repositorio de GitHub.")
+    # st.write("[Repositorio de GitHub](https://github.com/PabloZuVal/NeumDetect)")
 
-# Selección de modelo
-model_choice = st.selectbox("Seleccionar modelo", list(MODELS.keys()))
+with col2:
+    st.subheader("Instrucciones")
+    st.write("1. Carga una imagen de rayos X de tórax en formato .jpg o .png.")
+    st.write("2. Selecciona un modelo de clasificación.")
+    st.write("3. Haz clic en el botón 'Realizar predicción' para obtener el diagnóstico.")
 
-# Botón para realizar la predicción
-if st.button("Realizar predicción"):
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
+    # Carga de imagen
+    uploaded_file = st.file_uploader("Cargar imagen de rayos X", type=["jpg", "jpeg", "png"])
+with col3:
+    st.subheader("Modelos de clasificación")
+    st.write("Selecciona un modelo de clasificación para realizar la predicción.")
+    st.write("Cada modelo utiliza una arquitectura de red neuronal diferente para clasificar las imágenes.")
+    # Selección de modelo
+    model_choice = st.selectbox("Seleccionar modelo", list(MODELS.keys()))
 
-        # Realizar predicción con el modelo seleccionado
-        result = MODELS[model_choice](image)
+    # Botón para realizar la predicción
+    if st.button("Realizar predicción"):
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
 
-        if result:
-            col1, col2 = st.columns([1, 2])
+            # Realizar predicción con el modelo seleccionado
+            result = MODELS[model_choice](image)
 
-            with col1:
+            if result:
+                # col1, col2 = st.columns([1, 2])
+
+                # with col1:
                 st.image(image, caption="Imagen cargada", width=300)
 
                 st.write(f"Diagnóstico: {result['diagnosis']}")
                 st.write(f"Probabilidad: {result['probability']}")
-            # with col2:
-    else:
-        st.write("Por favor, carga una imagen antes de realizar la predicción.")
+                # with col2:
+        else:
+            st.write("Por favor, carga una imagen antes de realizar la predicción.")
